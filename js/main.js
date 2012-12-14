13034d7e2db2fc5335e595bb2f905ff8b9702ae8
Daniel Evans <danharibo@gmail.com>
2012-12-14 15:17:32 +0000

    Added countdown redirect

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function(){
+    function lol() {
+        var d = Date.now();
+        var target = new Date(2012, 11, 14, 16, 00, 00);
+        var del = 1000 * 30;
+        var delta = (target.getTime() - d);
+        if( del > delta )
+        {
+            window.location = 'http://www.youtube.com/watch?v=e32kaa9TzeE';
+        }
+        else
+        {
+            console.log('redirect in: ', delta - del, del );
+        }
+    }
+    setInterval(lol, 1000);
 
+});
