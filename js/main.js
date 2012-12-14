373baa87aa42261d4eab3c2d0d40fb81af1f0540
Daniel Evans <danharibo@gmail.com>
2012-12-14 15:46:44 +0000

    Updated timer

--- js/main.js
+++ js/main.js
@@ -4,13 +4,10 @@ $(document).ready(function(){
         var target = new Date(2012, 11, 14, 16, 00, 00);
         var del = 1000 * 30;
         var delta = (target.getTime() - d);
+        console.log(delta);
         if( del > delta && delta > 0 )
         {
             window.location = 'http://www.youtube.com/watch?v=e32kaa9TzeE';
-        }
-        else
-        {
-            console.log('redirect in: ', delta - del, del );
         }
     }
     setInterval(lol, 1000);
