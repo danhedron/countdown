a770441c5dc92b1529da3767577f8e457a537531
Daniel Evans <danharibo@gmail.com>
2012-12-14 15:39:30 +0000

    disabled redirect after timeout

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         var target = new Date(2012, 11, 14, 16, 00, 00);
         var del = 1000 * 30;
         var delta = (target.getTime() - d);
-        if( del > delta )
+        if( del > delta && delta > 0 )
         {
             window.location = 'http://www.youtube.com/watch?v=e32kaa9TzeE';
         }
